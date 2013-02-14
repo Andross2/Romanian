181efce3163747958bd8be530860fe6b0ba9b6b2
Luis Eduardo Hernandez Diaz <biz0505@hotmail.com>
2013-02-14 11:40:55 -0600

    Comments deleted
    Comments deleted

--- Romanian/main.cpp
+++ Romanian/main.cpp
@@ -16,10 +16,7 @@ int main(int argc, const char * argv[])
     int factores [13] = {1,5,10,50,100,500,1000,5000,10000,50000,10000,500000,1000000};
     char letras [13] = {'I','V','X','L','C','D','M','v','x','l','c','d','m'};
     
-    //int factores [13] = {1,5,10,50,100,500,1000,5000,10000,50000,10000,500000,1000000};
-    //string letras [13] = {'I','V','X','L','C','D','M','v','x','l','c','d','m'};
-    
-    int max = 13; //max(1,2);
+    int max = 13; 
     
     int arabic;
     
@@ -29,10 +26,8 @@ int main(int argc, const char * argv[])
     
     string result;
     
-    //for( int i = 0; i < max && temp_number > 0 ; i = i - 2  )
     for( int i = 0; i < max - 2 && temp_number > 0 ; i = i + 2  )
     {
-        //int pos = max - i - 1;
         int pos = i ;
         
         int last_digit = temp_number - ((temp_number / 10)*10);
@@ -41,70 +36,37 @@ int main(int argc, const char * argv[])
         //9
         if( temp == 9)
         {
-            //cout << letras[pos] << letras[pos+2];
             result = result + letras[pos+2] + letras[pos];
-            //result = result ;
             temp -= 9;
         }
         //4
         if( temp == 4)
         {
-            //cout << letras[pos+1] ;
             result = result  + letras[pos+1] + letras[pos];
             temp -= 4;
         }
         //1
         while( ( 9 > temp && temp > 5) || ( 4 > temp  && temp > 0 ) )
         {
-            //cout << letras[pos] ;
             result = result + letras[pos];
             temp--;
         }
         //5
         if( temp >= 5)
         {
-            //cout << letras[pos+1] ;
             result = result + letras[pos+1];
-            
             temp -= 5;
         }
         
         
         temp_number = temp_number / 10;
         
-        //        // 9
-        //        if( temp_number >= factores[pos - 1] )
-        //        {
-        //            cout << letras[pos-1] ;
-        //            temp_number -= factores[pos - 1];
-        //        }
-        //
-        //        if( temp_number >= factores[pos - 1] )
-        //        {
-        //            cout << letras[pos-1] ;
-        //            temp_number -= factores[pos - 1];
-        //        }
-        //
-        //        // 0-3 6-8
-        //        while( temp_number >= factores[pos] )
-        //        {
-        //            cout << letras[pos] ;
-        //            temp_number -= factores[pos];
-        //        }
-        
     }
     
     cout << endl;
     cout << arabic << " in romanian is:" << endl;
-    cout << string(result.rbegin(),result.rend()) << endl; // I google this
+    cout << string(result.rbegin(),result.rend()) << endl;
     
-    
-    // insert code here...
-    //string s;
-    //cin >> s;
-    //cout << s;
-    //std::cin >> s;
-    //std::cout << "Hello, World!\n";
     return 0;
 }
 
